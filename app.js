efa99ab4b8a9d46fd6928a5efcce6db865c4fe5e
Ashodiya, Kaushik <Kaushik.Ashodiya@sf.frb.org>
2015-11-30 18:56:48 -0800

    Db and Bootstrap added

--- app.js
+++ app.js
@@ -4,15 +4,31 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
+var dbUrl = 'mongodb://localhost:27017/k-vote-app';
+
+// Connect to database
+mongoose.connect(dbUrl);
+mongoose.connection.on('error', function(err) {
+  console.error('MongoDB connection error: ' + err);
+  process.exit(-1);
+  }
+);
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+
+//app.set('view engine', 'jade');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -21,6 +37,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Make our db accessible to our router
+//app.use(function(req,res,next){
+//  req.db = db;
+//  next();
+//});
 
 app.use('/', routes);
 app.use('/users', users);
